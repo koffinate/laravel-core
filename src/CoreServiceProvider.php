502a7ac36081ecb84bfed20a919404e75c05bdb6
Much Yusron Arif <3467343+yusronarif@users.noreply.github.com>
2022-11-17 17:23:17 +0700

    Create CoreServiceProvider.php

--- src/CoreServiceProvider.php
+++ src/CoreServiceProvider.php
@@ -15,8 +15,8 @@ class CoreServiceProvider extends ServiceProvider
      */
     public function register()
     {
-        $this->mergeConfigFrom(__DIR__.'/../config/yusronarif/core.php', 'yusronarif.core');
-        $this->mergeConfigFrom(__DIR__.'/../config/yusronarif/plugins.php', 'yusronarif.plugins');
+        $this->mergeConfigFrom(__DIR__.'/../config/koffinate/core.php', 'koffinate.core');
+        $this->mergeConfigFrom(__DIR__.'/../config/koffinate/plugins.php', 'koffinate.plugins');
 
         $this->app->register(DbServiceProvider::class, true);
         $this->app->register(BladeServiceProvider::class, true);
@@ -29,7 +29,7 @@ class CoreServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        $this->publishes([__DIR__.'/../config/yusronarif/core.php' => config_path('yusronarif/core.php')], 'config');
-        $this->publishes([__DIR__.'/../config/yusronarif/plugins.php' => config_path('yusronarif/plugins.php')], 'config');
+        $this->publishes([__DIR__.'/../config/koffinate/core.php' => config_path('koffinate/core.php')], 'config');
+        $this->publishes([__DIR__.'/../config/koffinate/plugins.php' => config_path('koffinate/plugins.php')], 'config');
     }
 }
